0057238c5e9ea1b7643c79ed4eeeba7f3956c90a
Md Abubakar <56873234+AbubakarMd@users.noreply.github.com>
2023-01-06 11:45:32 +0530

    Create func01.js

---
+++ func01.js
@@ -0,0 +1,38 @@
+// without parameter
+
+
+// function trial() {
+//     console.log("hey, i am in function!");
+// }
+
+// trial();
+// trial();
+
+
+
+
+
+// parameter
+
+
+// function param(name) {
+//     console.log("hey, this is first line");
+//     console.log("my name will be : " + name);
+// }
+
+// param("first parameter");
+// param("second parameter");
+
+
+
+
+// use of return
+
+function hello() {
+    return "welcome to github! ";
+}
+
+var hey = hello(); // we are storing value in variable   
+hello(); // this will not return value as we have not specified what to do with function
+console.log(hey); // we can pass variable in console
+console.log(hello()); // we can directly pass function in console.
