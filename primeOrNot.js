c7a7fe9280ce8f81ce6a2b4962079305b2a589ad
Md Abubakar <56873234+AbubakarMd@users.noreply.github.com>
2023-02-04 11:30:08 +0530

    Update primeOrNot.js

--- primeOrNot.js
+++ primeOrNot.js
@@ -1,1 +1,12 @@
 
+let n = 500;   //enter your own value
+let count = 0;
+for (i = 2; i < n; i++) {
+    if (n % i == 0) {
+        console.log(n + " is divisible by " + i);
+        count = 1;
+    }
+}
+if (count == 0) {
+    console.log(n + " is prime");
+}
